c71afb338b33b56e0199749d649ec0737c6571cc
Michael Tillman <mnamllit@gmail.com>
2022-03-24 17:56:53 -0500

    added a UI module, also added a public method to display the array onto the DOM.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+let board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 const player = (x) => {
     const marker = x
@@ -7,3 +7,23 @@ const player = (x) => {
 
 const x = player('X')
 const y = player('O')
+
+
+
+const ui = ( () => {
+    const gameDiv = document.getElementById('game-container')
+    const display = () => {
+        board.forEach( element => {
+            const cell = document.createElement('span')
+            cell.innerHTML = element
+            cell.classList.add('cell')
+            gameDiv.appendChild(cell)
+        })
+    }
+
+    return {display}
+})()
+
+
+ui.display()
+
