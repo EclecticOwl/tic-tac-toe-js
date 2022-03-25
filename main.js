55d0bf293c0197dbca661d07ea63fe5901031a03
Michael Tillman <mnamllit@gmail.com>
2022-03-25 00:05:56 -0500

    added game control flow

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-let board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+let board = ['', '', '', 
+            '', '', '', 
+            '', '', '']
 
 const player = (x) => {
     const marker = x
     return {marker}
 }
 
-const x = player('X')
-const y = player('O')
-
+let count = 0;
 
 
 const ui = ( () => {
     const gameDiv = document.getElementById('game-container')
+
+
     const display = () => {
         board.forEach( element => {
             const cell = document.createElement('span')
@@ -22,24 +24,63 @@ const ui = ( () => {
     }
     const clear = () => {
         const cell = document.querySelectorAll('.cell')
-        
         cell.forEach( element => {
             element.remove()
         })
         
     }
-    const input = () => {
+    const input = (player) => {
         const monitor = document.querySelectorAll('.cell')
+        const header = document.getElementById('header')
+        const msg = document.createElement('span')
+
+        player.marker == 'X' ? msg.textContent = 'Player 1 Turn' : msg.textContent = 'Player 2 Turn'
+
+
+        header.appendChild(msg)
+        
+        console.log('Player to play is:')
+        console.log(player.marker)
+
         monitor.forEach( (element, index) => {
             element.addEventListener('click', () => {
-                board[index] = 'X'
+                board[index] = player.marker
+                msg.textContent = ''
+                count++
+                gameLogic()
+
             })
         })
+        
     }
 
     return {display, clear, input}
 })()
 
+const gameLogic = () => {
+    const x = player('X')
+    const o = player('O')
 
-ui.display()
-ui.input()
+
+    const refresh = () => {
+        ui.clear()
+        ui.display()
+    }
+    console.log('Count:', count)
+
+    if (count > 9) {
+        console.log('End of game')
+    }else if (count == 0) {
+        ui.display()
+        ui.input(x)
+    }else if (count % 2 != 0) {
+        refresh()
+        ui.input(o)
+    }else if (count % 2 == 0) {
+        refresh()
+        ui.input(x)
+    }
+
+}
+
+gameLogic()
