6eb3f747e0441c8b2d5fab7bd8d9ff4608c40d16
Michael Tillman <mnamllit@gmail.com>
2022-03-25 01:14:39 -0500

    Added a function to validate winner.

--- main.js
+++ main.js
@@ -8,7 +8,23 @@ const player = (x) => {
 }
 
 let count = 0;
+let win = false
 
+const winCheck = () => {
+    if (win == false) {
+        //Checking for wins horizontally
+        if (board[0] == board[1] && board[0] == board[2] && board[0] != '') win = true
+        if (board[3] == board[4] && board[3] == board[5] && board[3] != '') win = true
+        if (board[6] == board[7] && board[6] == board[8] && board[6] != '') win = true
+        //Checking for wins vertically
+        if (board[0] == board[3] && board[0] == board[6] && board[0] != '') win = true
+        if (board[1] == board[4] && board[1] == board[7] && board[1] != '') win = true
+        if (board[2] == board[5] && board[2] == board[8] && board[2] != '') win = true
+        //Checking for wins diagonally
+        if (board[0] == board[4] && board[0] == board[8] && board[0] != '') win = true
+        if (board[2] == board[4] && board[2] == board[6] && board[2] != '') win = true
+    }
+}
 
 const ui = ( () => {
     const gameDiv = document.getElementById('game-container')
@@ -33,27 +49,24 @@ const ui = ( () => {
         const monitor = document.querySelectorAll('.cell')
         const header = document.getElementById('header')
         const msg = document.createElement('span')
+        msg.classList.add('msg')
 
-        player.marker == 'X' ? msg.textContent = 'Player 1 Turn' : msg.textContent = 'Player 2 Turn'
+        player.marker == 'X' && win == false ? msg.textContent = 'Player 1 Turn' : msg.textContent = 'Player 2 Turn'
 
 
         header.appendChild(msg)
-        
-        console.log('Player to play is:')
-        console.log(player.marker)
 
-        monitor.forEach( (element, index) => {
-            element.addEventListener('click', () => {
-                board[index] = player.marker
-                msg.textContent = ''
-                count++
-                gameLogic()
-
-            })
-        })
-        
+        if (win == false) {
+            monitor.forEach( (element, index) => {
+                element.addEventListener('click', () => {
+                    board[index] = player.marker
+                    msg.textContent = ''
+                    count++
+                    gameLogic()
+                })
+            })   
+        }
     }
-
     return {display, clear, input}
 })()
 
@@ -61,12 +74,19 @@ const gameLogic = () => {
     const x = player('X')
     const o = player('O')
 
-
     const refresh = () => {
         ui.clear()
         ui.display()
     }
-    console.log('Count:', count)
+    
+    winCheck()
+
+    if (win == true) {
+        const header = document.getElementById('header')
+        const span = document.createElement('span')
+        span.textContent = 'Winner!'
+        header.append(span)
+    }
 
     if (count > 9) {
         console.log('End of game')
@@ -80,7 +100,6 @@ const gameLogic = () => {
         refresh()
         ui.input(x)
     }
-
 }
 
 gameLogic()
